4e3e8e1d1be267a3a5b552be23f17beceb778c3a
Milan Rasche <theslipperytrout@gmail.com>
2024-04-11 22:32:35 +0200

    changed folder structer added code for teensy

---
+++ Teensy/PollingRateCheckForAnalogPin/PollingRateCheckForAnalogPin.ino
@@ -0,0 +1,33 @@
+const int max9814OutputPin = A0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int startTime = micros();
+  int readings[100000];
+
+  for (int i = 0; i < 100000; i++) {
+    readings[i] = analogRead(max9814OutputPin);
+  }
+
+  int computeTime = micros() - startTime;
+
+  float timePer = computeTime / 100000.0;
+  Serial.println(timePer);
+  float Hz = 1 / (timePer / 1000000);
+  Serial.println(Hz);
+
+  unsigned long sum = 0;
+  for (int i = 0; i < 100000; i ++ ) {
+    sum = sum + readings[i];
+  }
+  float average = sum / 100000;
+  Serial.println(average);
+
+  
+
+  delay(1000000);
+}
+
