74a4bf3fad2939e7a5aef511b0ed460fe1b1ea08
Milan Rasche <theslipperytrout@gmail.com>
2024-04-14 13:08:37 +0200

    average volume setup

--- ESP32/Connections/Average-Sound/Average-Sound.ino
+++ ESP32/Connections/Average-Sound/Average-Sound.ino
@@ -1,52 +1,54 @@
 #include "secret.h"
-#include <WiFi.h> 
-#include <WiFiClientSecure.h> 
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <stdlib.h>
 
-#define AudioPin 36
+#define AudioPin 34
 
 //Objects
-WiFiClientSecure askClient; //SSL Client
+WiFiClientSecure askClient;  //SSL Client
 //WiFiClient askClient; //Non-SSL Client, also remove the comments for askClient.setCACert(local_root_ca);
 
 PubSubClient client(askClient);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(ssid);
+  // Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+  // WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  // }
 
-  }
 
-
-  Serial.println(WiFi.localIP());
-  askClient.setCACert(local_root_ca); //If you use non SSL then comment out
-  Serial.println(client.setServer(mqtt_server, mqtt_port).connect(ClientID, username, mqttpass)) ;
-  //client.setCallback(callback);
-
+  // Serial.println(WiFi.localIP());
+  // askClient.setCACert(local_root_ca);  //If you use non SSL then comment out
+  // Serial.println(client.setServer(mqtt_server, mqtt_port).connect(ClientID, username, mqttpass));
+  // //client.setCallback(callback);
 }
 
 void loop() {
   int StartTime = micros();
-  
   int counter = 0;
-    //List or array here
+  long sum = 0;
 
-    while (micros() - StartTime < 20000) {
-
-    analogRead(AudioPin);
+  Serial.println("Start");
+  while (counter < 100000) {
+    int reading = analogRead(AudioPin);
+    sum = sum + (1360 + abs(1360 - reading));
     counter++;
-   }
-    //Caluclate Average
-    //Send average
-    
+  }
+  Serial.println("Finish");
 
-  delay();
+  //Caluclate Average
+  float average = float(sum) / float(counter);
+  float voltage = (((float(average) / 2720.0) * 3.3) - 1.65);
+  Serial.println(voltage);
+  Serial.println(average);
 
+  delay(10000);
 }
 
 // //MQTT callback
