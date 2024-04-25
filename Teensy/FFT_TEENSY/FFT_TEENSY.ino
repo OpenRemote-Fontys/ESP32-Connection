aaaf4b962f405e23ea1fcaebc0a9e9485ffd716a
Milan Rasche <theslipperytrout@gmail.com>
2024-04-26 00:18:47 +0200

    updated what gets sent

--- Teensy/FFT_TEENSY/FFT_TEENSY.ino
+++ Teensy/FFT_TEENSY/FFT_TEENSY.ino
@@ -2,8 +2,8 @@
 #include <arduinoFFT.h>
 
 //Variables to be used accross project
-#define SAMPLES 1024       //Number of samples take at a time, has to be a power of 2
-#define SAMPLE_FREQ 32000  //Interested in frequencuies up till 16kHz
+#define SAMPLES 2048       //Number of samples take at a time, has to be a power of 2
+#define SAMPLE_FREQ 40000  //Interested in frequencuies up till 20kHz
 #define AMPLITUDE 1000
 #define AUDIO_PIN A0
 #define NUMBER_BANDS 16  //The number of destinct frequency bands we want to record
@@ -50,63 +50,29 @@ void loop() {
   //Loot at FFT results
   for (int i = 2; i < (SAMPLES / 2); i++) {
     if (vReal[i] > NOISE) {
-      switch (i) {
-        case 0 ... 2:
-          bandValues[0] += (int)vReal[i];
-          break;
-        case 3 ... 3:
-          bandValues[1] += (int)vReal[i];
-          break;
-        case 4 ... 5:
-          bandValues[2] += (int)vReal[i];
-          break;
-        case 6 ... 7:
-          bandValues[3] += (int)vReal[i];
-          break;
-        case 8 ... 9:
-          bandValues[4] += (int)vReal[i];
-          break;
-        case 10 ... 13:
-          bandValues[5] += (int)vReal[i];
-          break;
-        case 14 ... 18:
-          bandValues[6] += (int)vReal[i];
-          break;
-        case 19 ... 25:
-          bandValues[7] += (int)vReal[i];
-          break;
-        case 26 ... 36:
-          bandValues[8] += (int)vReal[i];
-          break;
-        case 37 ... 50:
-          bandValues[9] += (int)vReal[i];
-          break;
-        case 51 ... 69:
-          bandValues[10] += (int)vReal[i];
-          break;
-        case 70 ... 97:
-          bandValues[11] += (int)vReal[i];
-          break;
-        case 98 ... 135:
-          bandValues[12] += (int)vReal[i];
-          break;
-        case 136 ... 189:
-          bandValues[13] += (int)vReal[i];
-          break;
-        case 190 ... 264:
-          bandValues[14] += (int)vReal[i];
-          break;
-        default:
-          bandValues[15] += (int)vReal[i];
-          break;
-      }
+      if (i > 2 && i <= 5) bandValues[0] += (int)vReal[i];
+      else if (i > 5 && i <= 7) bandValues[1] += (int)vReal[i];
+      else if (i > 7 && i <= 10) bandValues[2] += (int)vReal[i];
+      else if (i > 10 && i <= 14) bandValues[3] += (int)vReal[i];
+      else if (i > 14 && i <= 20) bandValues[4] += (int)vReal[i];
+      else if (i > 20 && i <= 28) bandValues[5] += (int)vReal[i];
+      else if (i > 28 && i <= 39) bandValues[6] += (int)vReal[i];
+      else if (i > 39 && i <= 55) bandValues[7] += (int)vReal[i];
+      else if (i > 55 && i <= 78) bandValues[8] += (int)vReal[i];
+      else if (i > 78 && i <= 109) bandValues[9] += (int)vReal[i];
+      else if (i > 109 && i <= 154) bandValues[10] += (int)vReal[i];
+      else if (i > 154 && i <= 218) bandValues[11] += (int)vReal[i];
+      else if (i > 218 && i <= 308) bandValues[12] += (int)vReal[i];
+      else if (i > 308 && i <= 434) bandValues[13] += (int)vReal[i];
+      else if (i > 434 && i <= 612) bandValues[14] += (int)vReal[i];
+      else if (i > 612) bandValues[15] += (int)vReal[i];
     }
   }
 
   for (int i = 0; i < NUMBER_BANDS; i++) {
     Serial1.print(bandValues[i]);
     Serial.print(bandValues[i]);
-    if(i < NUMBER_BANDS - 1) {
+    if (i < NUMBER_BANDS - 1) {
       Serial1.print(",");
       Serial.print(",");
     }
